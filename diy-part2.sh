fc7fee944ab6c9d8160ed994f1238107a3983e4e
OPPO518 <129146729+OPPO518@users.noreply.github.com>
2023-03-30 19:47:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,11 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='OpenWrt'/hostname='Nighthawk-X6'/g" package/base-files/files/bin/config_generate
 
-# Change Default Theme
-sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/-bootstrap/-argon/g' feeds/luci/collections/luci/Makefile
+# 取消原主题luci-theme-bootstrap 为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 argon 为默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 删除原默认主题
+rm -rf package/lean/luci-theme-bootstrap
