8bb8228b34183dc6d5f706f0f8ea786897274220
OPPO518 <129146729+OPPO518@users.noreply.github.com>
2023-03-31 19:22:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='Nighthawk-X6'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert hostname OK!========='
 
 # 取消原主题luci-theme-bootstrap 为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
