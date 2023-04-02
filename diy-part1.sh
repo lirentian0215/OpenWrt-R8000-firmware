e42f3d70acfe903f42ff6bf77b19eda6b9aa0507
OPPO518 <129146729+OPPO518@users.noreply.github.com>
2023-04-02 22:28:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,14 +20,18 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.de
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
-echo '添加jerrykuku的argon-mod主题'
-rm -rf package/lean/luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+# echo '添加jerrykuku的argon-mod主题'
+# rm -rf package/lean/luci-theme-argon  
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-echo '=========Add argon-mod OK!========='
+# echo '=========Add argon-mod OK!========='
 
 echo '移除bcm53xx中的其他机型'
 sed -i '393,453d' target/linux/bcm53xx/image/Makefile
 sed -i '352,384d' target/linux/bcm53xx/image/Makefile
 sed -i '140,344d' target/linux/bcm53xx/image/Makefile
 echo '=========Remove other devices of bcm53xx OK!========='
+
+# 添加gn 临时方案
+cd package
+svn co https://github.com/kenzok8/small-package/trunk/gn
